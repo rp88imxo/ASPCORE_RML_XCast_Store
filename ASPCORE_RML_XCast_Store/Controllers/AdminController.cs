00f02b1c5e7323048b7e7c69f4f39bef869477a1
rp88imxo <rp88imxo@gmail.com>
2023-01-12 00:16:32 +0300

    added admin panel view

---
+++ ASPCORE_RML_XCast_Store/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RMLXCast.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASPCORE_RML_XCast_Store/Controllers/AdminProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RMLXCast.Web.Controllers
+{
+    public class AdminProductsController : Controller
+    {
+        public IActionResult Products()
+        {
+            return View();
+        }
+    }
+}
