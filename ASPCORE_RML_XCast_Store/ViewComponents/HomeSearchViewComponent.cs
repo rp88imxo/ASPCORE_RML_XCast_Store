b3ea6a4e39260262ecaff4e709c72b13f819875c
rp88imxo <rp88imxo@gmail.com>
2023-01-30 21:32:42 +0300

    added search, ui changes on footer

--- ASPCORE_RML_XCast_Store/ViewComponents/HomeSearchViewComponent.cs
+++ ASPCORE_RML_XCast_Store/ViewComponents/HomeSearchViewComponent.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RMLXCast.Web.ViewModels.ShopProduct;
 
 namespace RMLXCast.Web.ViewComponents
 {
---
+++ ASPCORE_RML_XCast_Store/ViewModels/ShopProduct/SearchProductViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace RMLXCast.Web.ViewModels.ShopProduct
+{
+    public class SearchProductViewModel
+    {
+        public string? searchString { get; set; }
+        public int? categoryId { get; set; }
+    }
+}
